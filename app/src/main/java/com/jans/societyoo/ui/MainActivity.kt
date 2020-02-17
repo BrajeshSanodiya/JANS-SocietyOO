e49cad68943a18e6baca6098f9502be9ada803d8
Brajesh Sanodiya <sanodiya.brajesh@gmail.com>
2020-02-17 15:01:50 +0530

    - Connect UI flow till OTP screen

--- app/src/main/java/com/jans/societyoo/ui/MainActivity.kt
+++ app/src/main/java/com/jans/societyoo/ui/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_main)
 
         //val actionBar: ActionBar? = supportActionBar
